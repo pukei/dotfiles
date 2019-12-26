0d062fafa516215f33860fd44bdd9ef9077a9b29
paresharma <paresharma@gmail.com>
2019-12-26 15:09:22 +0900

    Start multiple servers

--- tmux.rb
+++ tmux.rb
@@ -6,16 +6,16 @@ begin
     bw: ['~/workspace/tripla_booking_widget', 2, 'yarn server --port 8080'],
     cb: ['~/workspace/triplabot2.0', 2, 'yarn server --port 8082'],
     cm: ['~/workspace/tripla_frontend_app', 2, 'yarn server --port 8083'],
-    ps: ['~/workspace/pakku_subbu', 3, 'br -p 7000'],
+    ps: ['~/workspace/pakku_subbu', 3, ['br -p 7000', 'anycable-go --host=localhost --port=7777 --path=/websocket', 'be anycable']],
     s:  ['~/workspace/ships', 3, 'br -p 4000'],
     sb: ['~/workspace/tripla_search_bar', 2, 'yarn server --port 8081'],
     sc: ['~/workspace/site-controller-api', 3, 'br -p 5000'],
     t:  ['~/workspace/tripla', 3, 'br']
   }.freeze
 
-  workspace = WORKSPACE[project][0]
+  workspace = '-c ' + WORKSPACE[project][0]
   more_windows = WORKSPACE[project][1]
-  start_server = WORKSPACE[project][2]
+  start_servers = Array(WORKSPACE[project][2])
 
 rescue
   msg = <<~TEXT
@@ -42,21 +42,33 @@ end
 
 `tmux start-server`
 
-`tmux new-session -d -s #{project} -n 1`
+`tmux new-session -d -s #{project} -n 1 #{workspace}`
 
 more_windows.times do |i|
-  `tmux new-window -t #{project}:#{i + 2} -n #{i + 2}`
+  `tmux new-window -t #{project}:#{i + 2} -n #{i + 2} #{workspace}`
 end
 
 (more_windows + 1).times do |i|
-  `tmux send-keys -t #{project}:#{i + 1} "cd #{workspace}" C-m`
-
   if i == 0
     `tmux send-keys -t #{project}:1 "vim" C-m`
   end
 
-  # last window
+  # Last window
   if i == more_windows
-    `tmux send-keys -t #{project}:#{i + 1} "#{start_server}" C-m`
+    start_servers.each_with_index do |start_server, si|
+      # Select pane
+      `tmux selectp -t #{project}:#{i + 1}`
+
+      if si == 1
+        `tmux split-window -h #{workspace}`
+      end
+
+      if si == 2
+        `tmux split-window -v #{workspace}`
+      end
+
+      # Run server
+      `tmux send-keys "#{start_server}" C-m`
+    end
   end
 end
